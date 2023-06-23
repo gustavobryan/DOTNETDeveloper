640270d30cbe7c58cd042987fe512bcbb069e3e3
Gustavo Bryan <devgustavob@gmail.com>
2023-06-23 16:42:18 -0300

    commit inicial

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using DOTNETDeveloper.Models;
+
+int a = 10;
+int b = 20;
+int c = a + b;
+
+Console.WriteLine(c);
+
+
+// DateTime dataAtual = DateTime.Now;
+// Console.WriteLine(dataAtual.ToString("dd/MM/yy HH:mm "));
+
+// string apresentacao = "Olá, seja bem vindo";
+// int quantidade = 1;
+// double altura = 1.57;
+// decimal preco = 1.89M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel quantidade: " + quantidade);
+// Console.WriteLine("Valor da variavel altura: " +altura);
+// Console.WriteLine("Valor da variavel preco: " + preco);
+// Console.WriteLine("Valor da variavel condicao: " + condicao);
+
+
+// Pessoa p = new Pessoa();
+
+// p.Nome = "Bryan";
+// p.Idade = 25;
+// p.Apresentar();
+

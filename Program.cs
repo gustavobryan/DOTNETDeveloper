6a81e6a4cd6e9b843c00ea1c8afb7431d6f2c3de
Gustavo Bryan <devgustavob@gmail.com>
2023-06-28 18:19:38 -0300

    incremento e decremento

--- Program.cs
+++ Program.cs
@@ -2,16 +2,29 @@
 
 Calculadora calc = new Calculadora();
 
-calc.Somar(10, 30);
-calc.Subtrair(10, 50);
-calc.Multiplicar(15, 5);
-calc.Dividir(2, 2);
-calc.Potencia(3, 3);
-calc.Seno(30);
-calc.Coseno(30);
-calc.Tangente(30);
+// calc.Somar(10, 30);
+// calc.Subtrair(10, 50);
+// calc.Multiplicar(15, 5);
+// calc.Dividir(2, 2);
+// calc.Potencia(3, 3);
+// calc.Seno(30);
+// calc.Coseno(30);
+// calc.Tangente(30);
 
+int numero = 10;
 
+Console.WriteLine(numero);
+
+Console.WriteLine("Incrementando o 10");
+numero ++;
+
+int numero2 = 20;
+
+Console.WriteLine("Decrementando o 20");
+numero2--;
+
+Console.WriteLine(numero);
+Console.WriteLine(numero2);
 
 
 // Console.WriteLine("Digite uma letra: ");
